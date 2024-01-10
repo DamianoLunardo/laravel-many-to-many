4de2ccefa1811c63871f429e6a622e10f93f6ef1
DamianoL <damianolunardo@hotmail.it>
2024-01-10 16:03:14 +0100

    Add - populated tech seeder

--- database/seeders/TechSeeder.php
+++ database/seeders/TechSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Tech;
 
 class TechSeeder extends Seeder
 {
@@ -12,6 +13,14 @@ class TechSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $tech = ['css', 'scss', 'html', 'vue', 'react', 'laravel', 'php', 'javascript', 'typescript'];
+
+        foreach ($tech as $tech_name) {
+            $tech = new Tech();
+            $tech->name = $tech_name;
+            $tech->save();
+        }
     }
+
+    
 }
